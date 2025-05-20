145117fc6841aa9d7cae5654a7b4aa47c8e4cb17
richardudeh4 <richardudeh03@gmail.com>
2025-05-20 11:01:50 +0100

    finished working on the navbar screen

---
+++ src/app/(home)/layout.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import {Navbar} from './Navbar';
+
+interface Props {
+    children:React.ReactNode;
+}
+
+const layout = ({children}:Props) => {
+  return (
+    <div className='flex flex-col min-h-screen'>
+       <Navbar/>
+        {children}
+    </div>
+  )
+}
+
+export default layout
